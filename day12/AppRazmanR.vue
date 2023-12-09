e9ae2f4f6da6a14ce0a3b29f63463f2b1920c411
razman <razmanhraman@gmail.com>
2023-12-10 07:15:02 +0800

    update=    Day12 Assignment Vue js   by RazmanR

---
+++ day12/AppRazmanR.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <AssignApp12 />
+
+
+  <!-- not called, coz HelloWorld not import -->
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+
+import AssignApp12 from "./components/AssignApp12.vue";
+
+export default {
+  name: 'App',
+  components: {
+
+
+
+
+    AssignApp12,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
