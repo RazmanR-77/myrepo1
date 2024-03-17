5a2995c56c9c375b4bd73ba5134f380a1c8af132
razman <razmanhraman@gmail.com>
2024-03-17 17:25:19 +0800

    init Assign Book System project

---
+++ Assignment1/src/main/java/org/example/assign1/Book.java
@@ -0,0 +1,72 @@
+package org.example.assign1;
+
+import java.time.LocalDate;
+
+public class Book {
+  private LocalDate IssueDate;
+  private LocalDate ExpectedReturnDate;
+  private LocalDate ReturnDate;
+  private String title;
+
+  //todo
+  String author;
+  private int bookID; // ISBN identifier for books
+  private int issuedTo;
+  public Boolean isIssued;
+
+  public  Book(String title, int bookId) {
+    // constructor OOP
+    this.title = title;
+    this.bookID = bookId;
+    this.isIssued = false;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getBookID() {
+    return bookID;
+  }
+
+  public void setIssuedTo(int issuedTo) {
+    this.issuedTo = issuedTo;
+  }
+
+  public int getIssuedTo() {
+    return issuedTo;
+  }
+
+  LocalDate getIssueDate() {
+    return IssueDate;
+  }
+
+  public LocalDate getExpectedReturnDate() {
+    return ExpectedReturnDate;
+  }
+
+//  void Issue(int id, Member member) {
+//    member.setIssued(this.bookID);
+//    member.canIssue = false;
+//    this.setIssuedTo(id);
+//    this.isIssued = true;
+//    IssueDate = LocalDate.now();
+//    ExpectedReturnDate = LocalDate.now().plusDays(7);
+//  }
+//
+//  public void ReturnBook(Member member){
+//    this.ReturnDate = LocalDate.now();
+//    this.isIssued = false;
+//    this.setIssuedTo(-1);
+//    member.setIssued(-1);
+//    member.canIssue = true;
+//  }
+
+  public void setReturnDate(LocalDate returnDate) {
+    this.ReturnDate = returnDate;
+  }
+
+  LocalDate getReturnDate() {
+    return ReturnDate;
+  }
+}
