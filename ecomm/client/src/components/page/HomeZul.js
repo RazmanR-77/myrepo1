6286d3273601d09c03c657758f523135a877a95a
razman <razmanhraman@gmail.com>
2024-04-07 17:57:03 +0800

    ecommerce client frontend

---
+++ ecomm/client/src/components/page/HomeZul.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../App.css'
+// import Cards from '../Cards';
+import HeroSection from '../HeroZul'
+import ProductList from '../ProductList';
+
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <ProductList />
+            {/* <Cards /> */}
+        </>
+    );
+}
+
+export default Home;
